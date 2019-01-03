db13ae0e94c381125caca0f16fcd85167d9871f5
Stacey Whitfield <staceywhitfield@Staceys-MacBook.local>
2019-01-03 13:06:35 -0500

    updated app.js CORS update

--- app.js
+++ app.js
@@ -26,6 +26,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 //app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 app.use('/api', api);
@@ -41,12 +49,7 @@ mongoose.connect
 (`Error Connecting to the Mongodb Database at URL :
  mongodb://127.0.0.1:27017/guestapp`)})
 
- app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
+ 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
